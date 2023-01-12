e4c6d810e679bfb4aeb0e85024f5b5ea630723c4
Akoudagerard <hodabaakounda@gmail.com>
2023-01-12 01:38:32 +0000

    initial commit

---
+++ ecommerce/shop/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey( Category, related_name = 'categorie', on_delete = models.CASCADE) 
+    image = models.CharField(max_length=5000)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+    def __str__(self):
+        return self.title
---
+++ ecommerce/shop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from shop.views import index, detail
+
+urlpatterns = [
+  path('', index, name='home'),
+  path('<int:myid>', detail, name="detail"),
+
+]
